ed9ca860eb6973348e9a3530a1e72341ff688343
Dergacz <sats96dergach@gmail.com>
2021-05-18 12:41:33 +0200

    add social network by lesson 12

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,15 @@
 import React from "react";
 import "./App.css";
+import {Header} from "./components/Header/Header";
+import {NavBar} from "./components/NavBar/NavBar";
+import {Profile} from "./components/Profile/Profile";
 
 function App() {
     return (
         <div className={"app_wrapper"}>
-            <header className={"header"}>
-                <img src={"https://cdn.logo.com/hotlink-ok/logo-social.png"}/>
-            </header>
-            <nav className={"nav"}>
-                <div>
-                    <a href={"#"}>Profile</a>
-                </div>
-                <div>
-                    <a href={"#"}>Messages</a>
-                </div>
-                <div>
-                    <a href={"#"}>News</a>
-                </div>
-                <div>
-                    <a href={"#"}>Music</a>
-                </div>
-                <div>
-                    <a href={"#"}>Settings</a>
-                </div>
-
-            </nav>
-            <div className={"content"}>
-
-            </div>
-
+            <Header/>
+            <NavBar/>
+            <Profile/>
         </div>
     );
 }
---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export const Profile = () => {
+    return (
+        <div className={"content"}>
+            <div>
+                <img
+                    src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyLW8hgwgOOeMZGdY_pZCsqrEkQotwFCK7NjUOqGiv0yDBRKS-8BlUwQ6Gv3h4ZjeKTFI&usqp=CAU"}/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My post
+                <div>
+                    new posts
+                    <div>
+                        <div>
+                            post 1
+                        </div>
+                        <div>
+                            post 2
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    )
+}
