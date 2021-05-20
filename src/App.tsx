6b5ee32509688507c01a01c73c63390d30570801
Dergacz <sats96dergach@gmail.com>
2021-05-20 11:23:08 +0200

    add social network by lesson 19

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import {Header} from "./components/Header/Header";
 import {NavBar} from "./components/NavBar/NavBar";
 import {Profile} from "./components/Profile/Profile";
 import {Dialogs} from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
+import {News} from "./components/News/News";
+import {Music} from "./components/Music/Music";
+import {Settings} from "./components/Settings/Settings";
 
 function App() {
     return (
@@ -11,8 +15,14 @@ function App() {
             <Header/>
             <NavBar/>
             <div className={"app_wrapper_content"}>
-                {/*<Profile/>*/}
-                <Dialogs/>
+                <BrowserRouter>
+                    <Route path={"/profile"} render={() => <Profile/>}/>
+                    <Route path={"/dialogs"} render={() => <Dialogs/>}/>
+                    <Route path={"/news"} render={() => <News/>}/>
+                    <Route path={"/music"} render={() => <Music/>}/>
+                    <Route path={"/settings"} render={() => <Settings/>}/>
+                </BrowserRouter>
+
             </div>
 
         </div>
